c1b7a705bf9e50935d21872e4532907e00e34024
TurnerBumbary <40765958+TurnerBumbary@users.noreply.github.com>
2018-07-04 08:54:45 -0500

    Add files via upload

--- CheckSumMC.java
+++ CheckSumMC.java
@@ -13,13 +13,14 @@ public class CheckSumMC {
 	private final AtomicInteger putSum = new AtomicInteger(0);
 	private final AtomicInteger takeSum = new AtomicInteger(0);
 	private final CyclicBarrier barrier;
-	private final int nTrials, nPairs;
+	private final int nTrials, nProducers;
+	private final AtomicInteger numThreads = new AtomicInteger(1);
 
 	public CheckSumMC() {
 		this.queue = new LinkedQueue<Integer>();
-		this.nTrials = 4;
-		this.nPairs = 1;
-		this.barrier = new CyclicBarrier(nPairs * 2 + 1);
+		this.nTrials = 10;
+		this.nProducers = 10;
+		this.barrier = new CyclicBarrier(nProducers + 2);
 	}
 
 	public static void main(String[] args) {
@@ -31,27 +32,22 @@ public class CheckSumMC {
 
 	public void test() {
 		try {
-			for (int i = 0; i < nPairs; i++) {
-			pool.execute(new Producer());
-			pool.execute(new Consumer());
+			for (int i = 0; i < nProducers; i++) {
+				pool.execute(new Producer());
 			}
-			System.out.println("Passed instantiation");
-			System.out.println();
-			barrier.await(); // wait for threads to be ready
-			System.out.println();
-			System.out.println("Currently Executing");
-			System.out.println();
+			pool.execute(new Monitor());
+			barrier.await(); // the main method waits for all of the other methods to execute
 			barrier.await(); // wait for threads to be finished
 			int sum1 = putSum.get();
 			int sum2 = takeSum.get();
 			System.out.println();
-			System.out.println("Put Sum: " + sum1);
-			System.out.println("Take Sum: " + sum2);
+			System.out.println("Producer Sum: " + sum1);
+			System.out.println("Monitor Sum: " + sum2);
 			System.out.println();
 			if (sum1 == sum2)
-				System.out.println("Put Sum & Get Sum are the same!");
+				System.out.println("Producer Sum & Monitor Sum are the same!");
 			else
-				System.out.println("Put Sum & Get Sum are different!");
+				System.out.println("Producer Sum & Monitor Sum are different!");
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -60,17 +56,19 @@ public class CheckSumMC {
 	class Producer implements Runnable {
 		public void run() {
 			try {
+				numThreads.getAndIncrement();
 				System.out.println("Producer is Running!");
 				int seed = this.hashCode();
 				int sum = 0;
 				for (int i = nTrials; i > 0; --i) {
 					queue.put(seed);
-					System.out.println("Put Queue: " + seed);
 					sum += seed;
 					// seed = xorShift(seed);
 				}
 				barrier.await();
 				putSum.getAndAdd(sum);
+				System.out.println("Current Put Sum: " + sum);
+				System.out.println("Partial Put Sum: " + putSum.get());
 				barrier.await();
 			} catch (Exception e) {
 				throw new RuntimeException(e);
@@ -78,20 +76,20 @@ public class CheckSumMC {
 		}
 	}
 
-	class Consumer implements Runnable {
+	class Monitor implements Runnable {
 		public void run() {
 			try {
-				System.out.println("Consumer is Running!");
+				numThreads.getAndIncrement();
+				System.out.println("Monitor is Running!");
 				int sum = 0;
 				barrier.await();
 				LinkedQueue.Node<Integer> travel = queue.getHead();
 				while (travel.next.get() != null) {
 					travel = travel.next.get();
 					int element = travel.item;
-					System.out.println("Get Queue: " + element);
 					sum += element;
 				}
-				takeSum.getAndAdd(sum);
+				takeSum.set(sum);
 				barrier.await();
 			} catch (Exception e) {
 				throw new RuntimeException(e);
